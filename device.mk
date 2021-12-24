0fe90fe74f4735dc63b28a9387e8bb218b6a6425
P-Salik <peersalik16@gmail.com>
2021-12-25 00:41:02 +0530

    RMX1805: Build android.hardware.camera.provider@2.4 from source
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -120,6 +120,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-service \
