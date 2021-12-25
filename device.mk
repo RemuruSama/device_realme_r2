833da6f641f303252a023d596c3fd2a2d0a64c42
P-Salik <peersalik16@gmail.com>
2021-12-25 09:46:50 +0530

    RMX1805: Build android.hardware.camera.common@1.0 from source
    * Fixes:
    E vndksupport: Could not load /vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so from sphal namespace: dlopen failed: library android.hardware.camera.common@1.0.so not found: needed by /vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so in namespace sphal.
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.3.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
