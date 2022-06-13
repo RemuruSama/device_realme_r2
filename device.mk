33d64d618097f06a7f279e59ef4682aa4214745b
RemuruSama <abhinavftp98@gmail.com>
2022-06-13 18:15:50 +0530

    RMX1805: drop thermal conf

--- device.mk
+++ device.mk
@@ -399,10 +399,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.msm8953
